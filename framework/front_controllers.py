5aebd4ac3b547021f8829609f604cb4e2831903b
Yanis Kachinskis <yanis.kachinskis@gmail.com>
2021-02-16 22:19:14 +0300

    done

--- framework/front_controllers.py
+++ framework/front_controllers.py
@@ -5,4 +5,14 @@ def get_token(request):
     request['token'] = random.randint(1000000, 9999999)
 
 
-front_controllers = [get_token]
+def get_menu(request):
+    links_menu = [
+        {'href':  '/', 'name':  'Главная'},
+        {'href':  '/contact/', 'name':  'Написать нам'},
+        {'href':  '/about/', 'name': 'О нас'},
+    ]
+    request['links_menu'] = links_menu
+
+
+
+front_controllers = [get_token, get_menu]
--- views.py
+++ views.py
@@ -2,18 +2,28 @@ from framework.templates import render
 
 
 def get_index_view(request):
-    token = request.get('token')
-    return '200 OK', render('index.html', token=token)
+    courses = [
+        {'name': 'Английский'},
+        {'name': 'Испанский'},
+        {'name': 'Китайский'},
+        {'name': 'Французский'}
+    ]
+    links_menu = request.get('links_menu')
+    return '200 OK', render('course_list.html', objects_list=courses,
+                            links_menu=links_menu)
 
 
 def get_about_view(request):
     adresses = ['Москва, ул. Ударная, 13',
                 'Санкт-Петербург, ул. Моховая, 23',
                 'Казань, пр. Мира, 10']
-    return '200 OK', render('about.html', object_list=adresses)
+    links_menu = request.get('links_menu')
+    return '200 OK', render('about.html', objects_list=adresses,
+                            links_menu=links_menu)
 
 
 def get_contact_view(request):
+    links_menu = request.get('links_menu')
     if request['method'] == 'POST':
         data = request['data']
         title = data['title']
@@ -25,5 +35,5 @@ def get_contact_view(request):
             f'Тема: {title}\n'
             f'Текст: {text}\n'
             f'++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
-        return '200 OK', render('contact.html')
-    return '200 OK', render('contact.html')
+        return '200 OK', render('contact.html', links_menu=links_menu)
+    return '200 OK', render('contact.html', links_menu=links_menu)
